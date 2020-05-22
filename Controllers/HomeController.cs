d107ea5fbe35214793a912c9497157903d9d0ddc
forkoshe <50763116+forkoshe@users.noreply.github.com>
2020-05-22 21:29:31 +0300

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication16.Controllers
+namespace Labka.Controllers
 {
     public class HomeController : Controller
     {
